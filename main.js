ab52261bbfb17d9342cf3aa80b2c232884bbf3e7
gnjaramillo <gnjaramillo1986@gmail.com>
2024-03-13 09:20:37 -0500

    maquetacion de lista de prodcutos html en js

--- main.js
+++ main.js
@@ -4,103 +4,115 @@ const iconMenuMobile =document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
 const menuCarritoIcon=document.querySelector('.navbar-shopping-cart');
 const aside=document.querySelector('.product-detail');
+const cardsContainer =document.querySelector('.cards-container');
 
 /* Alternar clases: El método toggle() 
 botones de mostrar/ocultar elementos. */
 
+menuEmail.addEventListener('click', toggDesktopMenu);
 
-
-// menuEmail.addEventListener('click', toggDesktopMenu);
-
-// function toggDesktopMenu(){
-//     const isAsideClose=aside.classList.contains('inactive')
-//     if (!isAsideClose){
-//         aside.classList.add('inactive')
-//     }
-    
-//         desktopMenu.classList.toggle('inactive') 	
-// }
-
-
-
-// iconMenuMobile.addEventListener('click', toggMobileMenu);
-
-// function toggMobileMenu(){
-//     const isAsideClose=aside.classList.contains('inactive')
-//     if (!isAsideClose){
-//         aside.classList.add('inactive')
-//     }
-    
-//         mobileMenu.classList.toggle('inactive') 	
-    
-// }
-
-
-
-// menuCarritoIcon.addEventListener('click', toggCarritoAside);
-
-// function toggCarritoAside() {
-//     // Verifica si el menú móvil está cerrado
-//     const isMobileMenuClose = mobileMenu.classList.contains('inactive');
-    
-//     // Verifica si el menú de escritorio está cerrado
-//     const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
-
-//     if (!isMobileMenuClose) {
-//         // Si el menú móvil está abierto, lo oculta al hacer clic en el icono del carrito
-//         mobileMenu.classList.add('inactive');
-//     } else if (!isDesktopMenuClose) {
-//         // Si el menú de escritorio está abierto, lo oculta al hacer clic en el icono del carrito
-//         desktopMenu.classList.add('inactive');
-//     } else {
-//         // Si tanto el menú móvil como el de escritorio están cerrados, alterna la visibilidad del menú aside al hacer clic en el carrito
-//         aside.classList.toggle('inactive');
-//     }
-// }
-
-//OTRA FORMA DE HACERLO....
-
-iconMenuMobile.addEventListener('click', function() {
-    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    closeAllMenus();
-    if (isMobileMenuClosed) {
-        mobileMenu.classList.remove('inactive');
-    } else {
-        mobileMenu.classList.add('inactive');
-    }
-});
-
-menuCarritoIcon.addEventListener('click', function() {
-    const isAsideClosed = aside.classList.contains('inactive');
-    closeAllMenus();
-    if (isAsideClosed) {
-        aside.classList.remove('inactive');
-    } else {
-        aside.classList.add('inactive');
-    }
-});
-
-menuEmail.addEventListener('click', function() {
-    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
-    closeAllMenus();
-    if (isDesktopMenuClosed) {
-        desktopMenu.classList.remove('inactive');
-    } else {
-        desktopMenu.classList.add('inactive');
-    }
-});
-
-// la clase inactive los oculta porque tiene la propiedad display none en css
-function closeAllMenus() {
-    desktopMenu.classList.add('inactive');
-    mobileMenu.classList.add('inactive');
-    aside.classList.add('inactive');
-    // Agrega aquí cualquier otro menú que necesite cerrarse cuando se abra uno nuevo
+function toggDesktopMenu(){
+    desktopMenu.classList.toggle('inactive') 
+    aside.classList.add('inactive')
+    mobileMenu.classList.add('inactive') 	
 }
 
 
+iconMenuMobile.addEventListener('click', toggMobileMenu);
+
+function toggMobileMenu(){
+    mobileMenu.classList.toggle('inactive') 
+    aside.classList.add('inactive')   
+    desktopMenu.classList.add('inactive');
+}
+
+menuCarritoIcon.addEventListener('click', toggCarritoAside);
+
+function toggCarritoAside() {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+        aside.classList.toggle('inactive');
+    
+}
+
+const productList=[];
+productList.push({
+    name:'bike',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+},
+
+{
+    name:'camara',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+},
+{
+    name:'compu',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
 
 
+/*
+   <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+         </div>
+        </div>
+    </div>
+*/
 
+/* construir el anterior elemento html llamado product-card con js , lo encapsulo en una fn*/
 
+function renderProducts(arr){
+
+    for (product of productList){
+        productCard= document.createElement('div');
+        productCard.classList.add('product-card');
+     
+        //product={name, price, image}
+     
+        const productImg=document.createElement('img');
+        productImg.setAttribute('src', product.image);
+     
+        const productInfo =document.createElement('div');
+        productInfo.classList.add('product-info');
+     
+        const productInfoDiv = document.createElement('div')
+     
+        const productPrice = document.createElement('p')
+        productPrice.innerText ='$'+ product.price;
+        const productName = document.createElement('p')
+        productName.innerText=product.name
+     
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+        
+     
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+     
+        productInfoFigure.appendChild(productImgCart)
+     
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+     
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+     
+        cardsContainer.appendChild(productCard)
+     }
+}
+
+renderProducts(productList); //la funcion recibe un parametro arr que en este caso seria mi 
+//lista de productos
 
